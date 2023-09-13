31d50c7c9cc62a549eb42648e3393448c76bcd40
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2023-09-13 18:43:17 -0300

    Better Update USP-Course-InternationalRelashionshipsR.r

--- USP-Course-InternationalRelashionshipsR.r
+++ USP-Course-InternationalRelashionshipsR.r
@@ -1,4 +1,4 @@
-#R
+kkkk#R
 
 2+2 #soma#
 8*2 #multiplicação#
@@ -114,3 +114,98 @@ m1 <- m0 + geom_polygon(aes(x=long, y=lat, group=group, fill= income_tax_rate)) 
 m2 <- m1 + geom_path(aes(x=long, y=lat, group=group), color='grey', size=.1)
 m3 <- m2 + scale_fill_gradient(low = "lightblue", high = "darkblue")
 m3
+
+
+  unnest_tokens(output = "words", input = texto)
+  texto_un
+
+#Obter os l�xicos do dicion�rio AFINN#
+
+AFINN <- get_sentiments("afinn")
+
+
+# Merge os dados: jun��o por interse��o, juntar texto_un com os lexicos e polaridade do pacote LexiconPT.#
+
+tb_sen <- inner_join(texto_un,
+                     AFINN[, c("word", "value")],
+                     by = c("words" = "word"))
+
+# Faz a agrega��o pela m�dia do sentimento por discurso das autoridades#
+
+tb <- tb_sen %>%
+  group_by(id, sigla, ano) %>%
+  summarise(media = mean(value),
+            n = n(),
+            sentiment = media/n)
+
+# Densidade de kernel do escore de sentimento dos pa�ses#
+ggplot(tb, aes(sentiment, colour = sigla)) +
+  geom_density() +
+  xlim(-0.1, 0.1)+
+  labs(x = "Sentimento", y = "Densidade")
+
+#Novo objeto com os anos nos dois pa�ses#
+
+tb2 <- tb_sen %>%
+  group_by(pais, ano) %>%
+  summarise(media = mean(value),
+            n = n(),
+            sentiment = media)
+        
+
+#Gr�fico desse novo objeto por nome e sentimento#
+
+
+tb2 %>%
+  ggplot( aes(x=ano, y=sentiment, group=pais, color=pais)) +
+  geom_line() +
+  scale_color_viridis(discrete = TRUE) +
+  ggtitle("Sentimento por Pot�ncia e Ano") +
+  ylab("Sentimento do Discurso")
+
+# Determina as frequ�ncias dos termos de polaridade n�o nula.
+
+tb_words4 <- tb_sen %>%
+  count(words, value, sigla, sort = TRUE) %>%
+  filter(value != 0)
+
+#Prepara o objeto para virar nuvem de palavra#
+
+tb_cloud <- tb_words4 %>%
+  spread(key = "sigla", value = "n", fill = 0) %>%
+  rename("China" = "CHI", "Estados Unidos" = "USA")
+tb_cloud
+
+tb3 <- as.data.frame(tb_cloud[, c("China", "Estados Unidos")])
+rownames(tb3) <- tb_cloud$words
+head(tb3)
+
+
+comparison.cloud(tb3,
+                 colors = c("red", "blue"),
+                 max.words = min(nrow(tb), 500))
+
+#An�lise de sentimento com o dicion�rio NRC#
+
+NRC <- get_sentiments("nrc")
+
+discurso_nrc <- inner_join(texto_un,
+                     NRC[, c("word", "sentiment")],
+                     by = c("words" = "word"))
+
+decade_mood <-  discurso_nrc %>%
+  filter(sigla != "NA" & !sentiment %in% c("positive", "negative")) %>%
+  count(sentiment, sigla) %>%
+  group_by(sigla, sentiment) %>%
+  summarise(sentiment_sum = sum(n)) %>%
+  ungroup()
+
+my_colors <- c("#E69F00", "#56B4E9", "#009E73", "#CC79A7", "#D55E00")
+grid.col = c("China" = my_colors[1], "USA" = my_colors[2], "anger" = "grey", "anticipation" = "grey", "disgust" = "grey", "fear" = "grey", "joy" = "grey", "sadness" = "grey", "surprise" = "grey", "trust" = "grey")
+
+circos.clear()
+
+circos.par(gap.after = c(rep(5, length(unique(decade_mood[[1]])) - 1), 15,
+                         rep(5, length(unique(decade_mood[[2]])) - 1), 15))
+chordDiagram(decade_mood,grid.col = grid.col, transparency = .2)
+title("Sentimentos Mobilizados por China e EUA no CSONU")
